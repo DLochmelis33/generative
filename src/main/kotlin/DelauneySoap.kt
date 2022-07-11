98741bc2b5618260b512c6d1c878c7f93a3d6c5c
dlochmelis33 <dlochmelis@gmail.com>
2022-07-11 14:06:10 +0300

    make soap less soap but moving

--- src/main/kotlin/DelauneySoap.kt
+++ src/main/kotlin/DelauneySoap.kt
@@ -4,9 +4,11 @@ import org.openrndr.color.ColorRGBa
 import org.openrndr.extra.noise.Random
 import org.openrndr.extra.triangulation.Delaunay
 import org.openrndr.math.Vector2
+import org.openrndr.shape.Segment
+import kotlin.math.pow
 
-const val optimalDistance = 150.0
-const val forceCoef = 0.15
+const val optimalDistance = 200.0
+const val forceCoef = 0.01
 
 fun main() = application {
     configure {
@@ -16,7 +18,7 @@ fun main() = application {
     }
     program {
         Random.seed = System.currentTimeMillis().toString()
-        val points = List(50) { drawer.bounds.center + drawer.bounds.center * Random.vector2() }.toMutableList()
+        val points = List(500) { drawer.bounds.center + drawer.bounds.center * Random.vector2() * 3.0 }.toMutableList()
         var userOverrideIndex: Int? = null
 
         mouse.buttonDown.listen { event ->
@@ -26,9 +28,20 @@ fun main() = application {
             userOverrideIndex = null
         }
 
+//        val edgesIdxPairs = points
+//            .flatMap { p -> List(3) { Segment(p, points.random()) } }
+//            .filter { it.length > 0.0001 }
+//            .map { Pair(points.indexOf(it.start), points.indexOf(it.end)) }
+        val edgesIdxPairs = Delaunay.from(points)
+            .triangles()
+            .flatMap { it.contour.segments }
+            .map { Pair(points.indexOf(it.start), points.indexOf(it.end)) }
+
         extend {
-            val triangles = Delaunay.from(points).triangles()
-            val edges = triangles.flatMap { it.contour.segments }
+//            val triangles = Delaunay.from(points).triangles()
+//            val edges = triangles.flatMap { it.contour.segments }
+            val edges: List<Segment> = edgesIdxPairs.map { Segment(points[it.first], points[it.second]) } +
+                    points.flatMap { p -> points.filter { (it - p).length < optimalDistance && it != p }.map { Segment(it, p) } }
 
             val forceSums = points.map { Vector2.ZERO }.toMutableList()
             for (e in edges) {
@@ -36,7 +49,9 @@ fun main() = application {
                 val ei = points.indexOf(e.end)
 
                 var f = e.end - e.start
-                f -= f.normalized * optimalDistance // force towards optimal distance
+                val opt = f.normalized * optimalDistance
+//                val dist = (f - opt).length
+                f -= opt // force towards optimal distance
 
                 forceSums[si] += f
                 forceSums[ei] -= f
@@ -49,12 +64,17 @@ fun main() = application {
 
             userOverrideIndex?.let { points[it] = mouse.position }
 
+            // artificial nudging
+            val ri1 = (seconds / 10.0 % points.size).toInt()
+            val ri2 = (seconds / 6.0 % points.size).toInt()
+            points[ri1] = points[ri2]
+
             with(drawer) {
                 stroke = ColorRGBa.WHITE
                 fill = ColorRGBa.PINK
                 strokeWeight = 0.1
                 segments(edges)
-                circles(points, 5.0)
+                circles(points, 3.0)
             }
         }
     }
